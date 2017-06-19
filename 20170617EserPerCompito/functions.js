7a3e17cb0e0f6e3dc20a3d5f84b4625cbdb908dc
Marco Antonino <antoninom@AULE.IT>
2017-06-19 19:22:12 +0200

    fatto un bellissimo form

--- 20170617EserPerCompito/functions.js
+++ 20170617EserPerCompito/functions.js
@@ -2,27 +2,50 @@ var form = document.getElementById("logSignForm");
 var inputs = document.getElementsByTagName("input");
 
 
-form.addEventListener("submit", function(event){
-  event.preventDefault(); //fermo lì, non fare l'azione predefinita
-  /*Si prende tutti gli elementi che hanno come tagname input*/
 
-  /**/
-  for (var i = 0; i < inputs.length; ++i){
-    //dando il punto accedo a type, name, value, ecc
-    if (inputs[i].value == "") {
-        console.log("non puoi lasciare vuoto il campo " + inputs[i].name);
-        //alert("Non puoi lasciare vuoto il campo " + inputs[i].name);
 
-      }else{
-      console.log(inputs[i].value);
-      //form.submit();
+form.addEventListener("submit",  function(event){
 
-      //sendToServer(inputs[i].value) come si fa nel mondo reale con ajax
-      //è una funzione scritta da noi
+
+
+  for (var i = 0; i < inputs.length; ++i) {
+    if(inputs[i].value == ""){
+      event.preventDefault();
+      inputs[i].placeholder = "NON puoi lasciare questo campo vuoto";
+      //inputs[i].className += "formInvalid";
+      inputs[i].className = "formInvalid";
+      //inputs[i].style.background= "red";
+
+      }
+
     }
+
   }
 
-});
+);
+
+
+// form.addEventListener("submit", function(event){
+//   event.preventDefault(); //fermo lì, non fare l'azione predefinita
+//   /*Si prende tutti gli elementi che hanno come tagname input*/
+//
+//   /**/
+//   for (var i = 0; i < inputs.length; ++i){
+//     //dando il punto accedo a type, name, value, ecc
+//     if (inputs[i].value == "") {
+//         console.log("non puoi lasciare vuoto il campo " + inputs[i].name);
+//         //alert("Non puoi lasciare vuoto il campo " + inputs[i].name);
+//
+//       }else{
+//       console.log(inputs[i].value);
+//       //form.submit();
+//
+//       //sendToServer(inputs[i].value) come si fa nel mondo reale con ajax
+//       //è una funzione scritta da noi
+//     }
+//   }
+//
+// });
 
 // form.addEventListener("blur", myBlurFunction, true);
 //
@@ -38,17 +61,22 @@ form.addEventListener("submit", function(event){
 //
 //     }
 
-function disableButton() {
+//function disableButton() {
 
-  for (var i = 0; i < inputs.length; i++) {
-    if(inputs[i].value.length == 0)
-    {
-      document.getElementById('button').disabled = true;
-    }
-    else {
-    document.getElementById('button').disabled = false;
-    }
-  }
-}
+  // for (var i = 0; i < inputs.length; i++) {
+  //   if(inputs[i].value.length == 0)
+  //   {
+  //     document.getElementById('button').disabled = true;
+  //   }
+  //   else {
+  //   document.getElementById('button').disabled = false;
+  //   }
+  // }
+//}
 
 //disableButton();
+
+function valid() {
+    event.srcElement.className = "formValid";
+    console.log(event.srcElement.id);
+}
